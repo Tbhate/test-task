5f336d501e80b242f27fb9f8f646163b5c646d77
Tbhate <dmitriybut83@gmail.com>
2025-05-24 00:16:21 +0300

    first commit)

---
+++ src/script.js
@@ -0,0 +1,23 @@
+const targetDate = new Date("2025-05-25T12:00:00").getTime();
+
+function updateCountdown() {
+    const now = new Date().getTime();
+    const distance = targetDate - now;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 *24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("days").innerText = String(days).padStart(2, '0');
+    document.getElementById("hours").innerText = String(hours).padStart(2, '0');
+    document.getElementById("minutes").innerText = String(minutes).padStart(2, '0');
+    document.getElementById("seconds").innerText = String(seconds).padStart(2, '0');
+
+    if (distance < 0){
+        document.getElementById("countdown").innerText = "00:00:00:00";
+        clearInterval(timerInterval)
+    }
+}
+
+const timerInterval = setInterval(updateCountdown, 1000);
