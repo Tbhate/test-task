4a443c0faf7d2af01b9d214d7ac81a5df5c8c98a
Tbhate <dmitriybut83@gmail.com>
2025-05-25 00:03:41 +0300

    adaptiv and modal window

--- src/script.js
+++ src/script.js
@@ -21,3 +21,24 @@ function updateCountdown() {
 }
 
 const timerInterval = setInterval(updateCountdown, 1000);
+
+
+// modal window
+
+  const modal = document.getElementById("modal");
+  const openBtn = document.getElementById("openModalBtn");
+  const closeBtn = document.getElementById("closeModalBtn");
+
+  openBtn.addEventListener("click", () => {
+    modal.style.display = "flex";
+  });
+
+  closeBtn.addEventListener("click", () => {
+    modal.style.display = "none";
+  });
+
+  window.addEventListener("click", (e) => {
+    if (e.target === modal) {
+      modal.style.display = "none";
+    }
+  });
